ab7a8b42d6b9e88e1f81ded3daf375b300041704
Parisa Pourmand <44519907+parisapourmand@users.noreply.github.com>
2021-07-14 23:38:30 +0430

    Add files via upload

---
+++ advertisment.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE Advertisment;
+
+CREATE TABLE BaseAdvertising(
+   id INT NOT NULL,
+   clicks INT,
+   views INT,
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE Advertiser(
+   name VARCHAR(1000) NOT NULL,
+   TOTALClicks INT,
+   id INT NOT NULL,
+   FOREIGN KEY (id) REFERENCES BaseAdvertising(id),
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE Advertiser(
+   title VARCHAR(1000) NOT NULL,
+   imgURL VARCHAR(1000) NOT NULL,
+   link VARCHAR(1000) NOT NULL,
+   id INT NOT NULL,
+   adviserId INT NOT NULL,
+   FOREIGN KEY (id) REFERENCES BaseAdvertising(id),
+   FOREIGN KEY (adviserId) REFERENCES Advertiser(id),
+   PRIMARY KEY(id)
+);
